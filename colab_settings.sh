be7511eaa05488ffd81d8e047f4f96efefe8c4d5
gnom2134 <gnom21345@gmail.com>
2022-03-24 12:44:56 +0300

    Add script for colab configuration and little fixes

---
+++ colab_settings.sh
@@ -0,0 +1,3 @@
+mkdir -p SouthParkData
+wget -qO ./SouthParkData/All-seasons.csv "https://github.com/BobAdamsEE/SouthParkData/raw/0deb9704677a073d1e3a1d19ec6070ef67b74803/All-seasons.csv"
+wget -qO ./inputs.py "https://raw.githubusercontent.com/gnom2134/NorthGarden/preprocessing/src/preprocessing/inputs.py"
